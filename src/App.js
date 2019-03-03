f2e42a31203129692f6455ed8001a34c99e202f4
shivam123425 <shivam123425@gmail.com>
2019-03-04 00:40:19 +0530

    Add table to display contacts, add methods to add and delete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Contacts from "./Components/Contacts/Contacts";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [{ id: 0, name: "Shivam", phone: "9891866484" }]
+    };
+  }
+  addContact = contact => {
+    const { name, phone } = contact;
+    const newContacts = [...this.state.contacts];
+    newContacts.push({ name, phone, id: newContacts.length + 1 });
+    this.setState({
+      contacts: newContacts
+    });
+  };
+  deleteContact = id => {
+    console.log(id);
+    const newContacts = this.state.contacts.filter(
+      contact => contact.id !== id
+    );
+    this.setState({
+      contacts: newContacts
+    });
+  };
   render() {
     return (
       <div className="App">
-        Hello World
+        <header className="header">Phone Directory</header>
+        <Contacts list={this.state.contacts} onDelete={this.deleteContact} />
       </div>
     );
   }
---
+++ src/Components/Contacts/Contacts.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const contacts = props => {
+  const contactList = props.list.map((contact, id) => {
+    return (
+      <tr key={id}>
+        <td>{contact.name}</td>
+        <td>{contact.phone}</td>
+        <td>
+          <button
+            onClick={() => props.onDelete(contact.id) || console.log("Deleted")}
+          >
+            DELETE
+          </button>
+        </td>
+      </tr>
+    );
+  });
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>NAME</th>
+          <th>PHONE</th>
+        </tr>
+        {contactList}
+      </tbody>
+    </table>
+  );
+};
+
+export default contacts;
